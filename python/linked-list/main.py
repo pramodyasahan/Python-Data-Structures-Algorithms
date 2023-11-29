bdacf634caa6e2a49a5402b352744a2b7ea874ff
pramodyasahan <sahandil1013@gmail.com>
2023-11-29 15:48:34 +0530

    Create: Append

--- python/linked-list/main.py
+++ python/linked-list/main.py
@@ -17,9 +17,20 @@ class LinkedList:
             print(temp.value)
             temp = temp.next
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
 
 my_linked_list = LinkedList(4)
+my_linked_list.append(5)
+my_linked_list.append(6)
 
-print('Head:', my_linked_list.head.value)
-print('Tail:', my_linked_list.tail.value)
-print('Length:', my_linked_list.length)
+print(my_linked_list.print_list())
