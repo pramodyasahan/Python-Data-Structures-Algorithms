d32fc4f8882e561c2d2100e62c5be8d00fc9aca4
pramodyasahan <sahandil1013@gmail.com>
2023-11-29 19:17:00 +0530

    Create: Remove

--- python/linked-list/main.py
+++ python/linked-list/main.py
@@ -99,9 +99,24 @@ class LinkedList:
         self.length += 1
         return True
 
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
 
 my_linked_list = LinkedList(0)
 my_linked_list.append(2)
 my_linked_list.insert(1, 1)
+my_linked_list.remove(2)
 
 my_linked_list.print_list()
