a0326e09e127941e28b7c4fde6ba451d2b61ad25
pramodyasahan <sahandil1013@gmail.com>
2023-11-29 16:23:15 +0530

    Create: Pop

--- python/linked-list/main.py
+++ python/linked-list/main.py
@@ -28,9 +28,29 @@ class LinkedList:
         self.length += 1
         return True
 
+    def pop(self):
+        if self.length == 0:
+            return None
+        else:
+            temp = self.head
+            pre = self.head
+            while temp.next:
+                pre = temp
+                temp = temp.next
+            self.tail = pre
+            self.tail.next = None
+            self.length -= 1
+            if self.length == 0:
+                self.head = None
+                self.tail = None
+            return temp.value
+
 
 my_linked_list = LinkedList(4)
 my_linked_list.append(5)
 my_linked_list.append(6)
+my_linked_list.pop()
+my_linked_list.pop()
+my_linked_list.pop()
 
 print(my_linked_list.print_list())
