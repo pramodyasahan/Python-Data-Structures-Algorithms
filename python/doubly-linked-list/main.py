7682cde3dbf5494b62104077e9f6e8fe198d5fbd
pramodyasahan <sahandil1013@gmail.com>
2023-11-30 23:16:17 +0530

    Create: Pop

--- python/doubly-linked-list/main.py
+++ python/doubly-linked-list/main.py
@@ -30,6 +30,23 @@ class DoublyLinkedList:
         self.length += 1
         return True
 
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp
+
 
 my_doubly_linked_list = DoublyLinkedList(7)
+my_doubly_linked_list.append(8)
+my_doubly_linked_list.append(9)
+my_doubly_linked_list.pop()
 my_doubly_linked_list.print_list()
