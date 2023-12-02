e6383868e47f42f4d05a44e4a2c94d78ff7ed1ff
pramodyasahan <sahandil1013@gmail.com>
2023-12-02 17:22:59 +0530

    Create: Insert method

--- python data-structures & algorithms/binary-search-tree/main.py
+++ python data-structures & algorithms/binary-search-tree/main.py
@@ -8,3 +8,23 @@ class Node:
 class BinarySearchTree:
     def __init__(self):
         self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while True:
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
