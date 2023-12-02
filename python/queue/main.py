b6d8c13c1b058164be38ff57c099b86635364ef7
pramodyasahan <sahandil1013@gmail.com>
2023-12-02 14:09:32 +0530

    Create: Enqueue method

--- python/queue/main.py
+++ python/queue/main.py
@@ -16,3 +16,14 @@ class Queue:
         while temp is not None:
             print(temp.value)
             temp = temp.next
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+        return True
