57a57a2bb682b8074eeeb51726bdf40e9f53fe5a
pramodyasahan <sahandil1013@gmail.com>
2023-12-02 13:39:24 +0530

    Create: Queue class

--- python/queue/main.py
+++ python/queue/main.py
@@ -4,3 +4,9 @@ class Node:
         self.next = None
 
 
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
