a2766806ab648f5a4610a4f625541de33f8514d5
pramodyasahan <sahandil1013@gmail.com>
2023-12-03 15:43:36 +0530

    Create: Get item method

--- python data-structures & algorithms/hash-table/main.py
+++ python data-structures & algorithms/hash-table/main.py
@@ -17,3 +17,11 @@ class HashTable:
         if self.data_map[index] is None:
             self.data_map[index] = []
         self.data_map[index].append([key, value])
+
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
