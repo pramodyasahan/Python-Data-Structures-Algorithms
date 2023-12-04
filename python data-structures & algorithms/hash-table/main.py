1119b8fff1fb29679f7380ca5f29ea3fb964b65d
pramodyasahan <sahandil1013@gmail.com>
2023-12-04 22:07:27 +0530

    Create: Keys method

--- python data-structures & algorithms/hash-table/main.py
+++ python data-structures & algorithms/hash-table/main.py
@@ -8,7 +8,7 @@ class HashTable:
             my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
         return my_hash
 
-    def prnt_table(self):
+    def print_table(self):
         for i, val in enumerate(self.data_map):
             print(i, ": ", val)
 
@@ -25,3 +25,11 @@ class HashTable:
                 if self.data_map[index][i][0] == key:
                     return self.data_map[index][i][1]
         return None
+
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
