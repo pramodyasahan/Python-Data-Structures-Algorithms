d218051e427c166a1e37bea79a1b2c104e632610
pramodyasahan <sahandil1013@gmail.com>
2023-12-04 22:21:07 +0530

    Create: Remove edge method

--- python data-structures & algorithms/graph/main.py
+++ python data-structures & algorithms/graph/main.py
@@ -18,3 +18,10 @@ class Graph:
             self.adj_list[vertex2].append(vertex1)
             return True
         return False
+
+    def remove_edge(self, vertex1, vertex2):
+        if vertex1 in self.adj_list.keys() and vertex2 in self.adj_list.keys():
+            self.adj_list[vertex1].remove(vertex2)
+            self.adj_list[vertex2].remove(vertex1)
+            return True
+        return False
