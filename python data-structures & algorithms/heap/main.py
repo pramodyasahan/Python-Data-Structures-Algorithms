34392c1bf678f5848d4e9adc0b4b47f2a232f6df
pramodyasahan <sahandil1013@gmail.com>
2023-12-05 15:43:26 +0530

    Create: Sink down method

--- python data-structures & algorithms/heap/main.py
+++ python data-structures & algorithms/heap/main.py
@@ -22,6 +22,24 @@ class MaxHeap:
             self._swap(current, self._parent(current))
             current = self._parent(current)
 
+    def _sink_down(self, index):
+        max_index = index
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+
+            if left_index < len(self.heap) and self.heap[left_index] > self.heap[max_index]:
+                max_index = left_index
+
+            if right_index < len(self.heap) and self.heap[right_index] > self.heap[max_index]:
+                max_index = left_index
+
+            if max_index != index:
+                self._swap(index, max_index)
+                index = max_index
+            else:
+                return
+
     def remove(self):
         if len(self.heap) == 0:
             return False
