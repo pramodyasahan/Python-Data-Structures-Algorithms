cd2807ed1a3898e326de48009ffb87738ad94b5e
pramodyasahan <sahandil1013@gmail.com>
2023-12-05 15:25:18 +0530

    Create: Remove method

--- python data-structures & algorithms/heap/main.py
+++ python data-structures & algorithms/heap/main.py
@@ -21,3 +21,15 @@ class MaxHeap:
         while current > 1 and self.heap[current] > self.heap[self._parent(current)]:
             self._swap(current, self._parent(current))
             current = self._parent(current)
+
+    def remove(self):
+        if len(self.heap) == 0:
+            return False
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+
+        return max_value
