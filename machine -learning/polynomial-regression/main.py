550fdcf1897de7b2c90a08b1f6eea3d2fc444150
pramodyasahan <sahandil1013@gmail.com>
2023-12-08 20:48:51 +0530

    import the data set

--- machine -learning/polynomial-regression/main.py
+++ machine -learning/polynomial-regression/main.py
@@ -2,3 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+dataset = pd.read_csv("Position_Salaries.csv")
+
+x = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
