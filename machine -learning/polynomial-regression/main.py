e454bb33a7563cea69a2c12263dd854d492a4168
pramodyasahan <sahandil1013@gmail.com>
2023-12-09 16:10:20 +0530

    Create: Linear regressor

--- machine -learning/polynomial-regression/main.py
+++ machine -learning/polynomial-regression/main.py
@@ -1,8 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 dataset = pd.read_csv("Position_Salaries.csv")
 
-x = dataset.iloc[:, 1:-1].values
+X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
+
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
