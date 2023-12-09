9e1afe56eb7267277550a2f62b93961845992fbf
pramodyasahan <sahandil1013@gmail.com>
2023-12-09 16:40:08 +0530

    Create: Polynomial regressor

--- machine -learning/polynomial-regression/main.py
+++ machine -learning/polynomial-regression/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
 dataset = pd.read_csv("Position_Salaries.csv")
 
@@ -10,3 +11,8 @@ y = dataset.iloc[:, -1].values
 
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
+
+poly_reg = PolynomialFeatures(degree=2)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_poly = LinearRegression()
+lin_reg_poly.fit(X_poly, y)
