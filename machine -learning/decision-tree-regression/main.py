67e6b3ea8b5eeb4830d234b75f3b5beeb5cc0886
pramodyasahan <sahandil1013@gmail.com>
2023-12-10 18:35:59 +0530

    Plot the values

--- machine -learning/decision-tree-regression/main.py
+++ machine -learning/decision-tree-regression/main.py
@@ -6,4 +6,15 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
-regressor = DecisionTreeRegressor(max_depth=2)
+regressor = DecisionTreeRegressor(max_depth=5)
+regressor.fit(X, y)
+
+y_pred = regressor.predict([[6.5]])
+print(y_pred)
+
+plt.scatter(X, y, color='red')
+plt.plot(X, regressor.predict(X), color='blue')
+plt.title('Decision Tree Regressor')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
