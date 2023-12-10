209773234e2c0e575c9a2c8b20673390f43d4c7f
pramodyasahan <sahandil1013@gmail.com>
2023-12-10 22:08:07 +0530

    Import dataset

--- machine -learning/random-forest-regression/main.py
+++ machine -learning/random-forest-regression/main.py
@@ -2,3 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
