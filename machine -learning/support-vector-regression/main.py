4497a8caf434d4725a8eefb0cb7cc673cba3903b
pramodyasahan <sahandil1013@gmail.com>
2023-12-10 18:15:16 +0530

    Plotting the regression model

--- machine -learning/support-vector-regression/main.py
+++ machine -learning/support-vector-regression/main.py
@@ -5,9 +5,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
-
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
 y = y.reshape(len(y), 1)
 
 sc_X = StandardScaler()
@@ -15,10 +14,14 @@ sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
 y = sc_y.fit_transform(y)
 
-y = np.ravel(y)
-
 regressor = SVR(kernel='rbf')
 regressor.fit(X, y)
 
-predicted_salary = sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])).reshape(-1, 1))
-print(predicted_salary)
+sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])).reshape(-1, 1))
+
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')
+plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X).reshape(-1, 1)), color='blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
