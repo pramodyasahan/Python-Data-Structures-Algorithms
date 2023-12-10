74e10ac08da3ee1f9198bb7a003b42fb2b99c3d7
pramodyasahan <sahandil1013@gmail.com>
2023-12-10 16:53:21 +0530

    Training the SVR model

--- machine -learning/support-vector-regression/main.py
+++ machine -learning/support-vector-regression/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
 
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
@@ -12,3 +13,6 @@ sc_X = StandardScaler()
 sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
 y = sc_y.fit_transform(y)
+
+regressor = SVR(kernel='rbf')
+regressor.fit(X, y)
