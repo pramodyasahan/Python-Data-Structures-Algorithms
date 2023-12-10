56d2a29f32729bac58d3af42d702f2ca32fc7848
pramodyasahan <sahandil1013@gmail.com>
2023-12-10 18:33:25 +0530

    Creat: Decision tree regressor

--- machine -learning/decision-tree-regression/main.py
+++ machine -learning/decision-tree-regression/main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeRegressor
 
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
+
+regressor = DecisionTreeRegressor(max_depth=2)
