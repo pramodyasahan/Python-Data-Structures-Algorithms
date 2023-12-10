7c67e94b86982733b6b57a08e4c425a9bdf17888
pramodyasahan <sahandil1013@gmail.com>
2023-12-10 18:17:25 +0530

    Finish: Support vector regression

--- machine -learning/support-vector-regression/main.py
+++ machine -learning/support-vector-regression/main.py
@@ -1,26 +1,35 @@
+# Importing necessary libraries
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 
+# Loading the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:-1].values
-y = dataset.iloc[:, -1].values
-y = y.reshape(len(y), 1)
 
+# Extracting the independent (X) and dependent (y) variables
+X = dataset.iloc[:, 1:-1].values  # Position levels
+y = dataset.iloc[:, -1].values  # Salaries
+y = y.reshape(len(y), 1)  # Reshaping y into a 2D array for scaling
+
+# Feature Scaling
 sc_X = StandardScaler()
 sc_y = StandardScaler()
-X = sc_X.fit_transform(X)
-y = sc_y.fit_transform(y)
+X = sc_X.fit_transform(X)  # Scaling X
+y = sc_y.fit_transform(y)  # Scaling y
 
-regressor = SVR(kernel='rbf')
-regressor.fit(X, y)
+# Creating and fitting the SVR model to the dataset
+regressor = SVR(kernel='rbf')  # Using Radial Basis Function (RBF) kernel
+regressor.fit(X, y)  # Fitting the model
 
-sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])).reshape(-1, 1))
+# Making a prediction for a 6.5 position level
+# Transforming 6.5 to the scaled value, predicting and then inverse transforming the prediction
+predicted_salary = sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])).reshape(-1, 1))
 
-plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')
-plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X).reshape(-1, 1)), color='blue')
+# Plotting the results
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')  # Actual data points
+plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X).reshape(-1, 1)),
+         color='blue')  # SVR predictions
 plt.title('Truth or Bluff (SVR)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
