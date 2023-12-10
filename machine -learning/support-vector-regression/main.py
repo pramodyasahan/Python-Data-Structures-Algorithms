4d353624e761e578b92ade4a8ca219764b0bc926
pramodyasahan <sahandil1013@gmail.com>
2023-12-10 16:46:11 +0530

    Apply feature scaling

--- machine -learning/support-vector-regression/main.py
+++ machine -learning/support-vector-regression/main.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
+y = y.reshape(len(y), 1)
+
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
