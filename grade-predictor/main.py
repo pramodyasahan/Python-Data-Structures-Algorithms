59305a928364e4d466fc60f46c601b39306f7b52
pramodyasahan <sahandil1013@gmail.com>
2023-12-11 16:45:15 +0530

    apply column transfer

--- grade-predictor/main.py
+++ grade-predictor/main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LinearRegression
 
 dataset = pd.read_csv('student-mat.csv', delimiter=';')
@@ -8,4 +11,5 @@ features_index = [8, 9, 13, 29, 30, 31]
 X = dataset.iloc[:, features_index].values
 y = dataset.iloc[:, -1].values
 
-
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0, 1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
