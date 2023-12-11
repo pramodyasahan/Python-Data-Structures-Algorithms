a0577a112ce9dbe0450fa6022770a51815b48b3f
pramodyasahan <sahandil1013@gmail.com>
2023-12-11 16:38:04 +0530

    import dataset

--- grade-predictor/main.py
+++ grade-predictor/main.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-
+dataset = pd.read_csv('student-mat.csv', delimiter=';')
+features_index = [8, 9, 13, 29, 30, 31]
+X = dataset.iloc[:, features_index].values
+y = dataset.iloc[:, -1].values
 
 
